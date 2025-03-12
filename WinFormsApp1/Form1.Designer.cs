1a248790a568b2d0350b208a1b07a543fbb09556
CpE Lab BE216 <CpE Lab BE216@BE216-PC05>
2025-03-12 09:09:13 +0800

    Version 1.0.0.1

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -36,9 +36,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(363, 182);
             label1.Name = "label1";
-            label1.Size = new Size(74, 15);
+            label1.Size = new Size(52, 15);
             label1.TabIndex = 0;
-            label1.Text = "Not updated";
+            label1.Text = "Updated";
             // 
             // Form1
             // 
