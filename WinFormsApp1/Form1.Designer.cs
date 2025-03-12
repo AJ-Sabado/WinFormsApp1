ef5bd5583577053b2aa929d906a5a98f777ae8ac
CpE Lab BE216 <CpE Lab BE216@BE216-PC05>
2025-03-12 08:39:56 +0800

    Test

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(357, 159);
+            label1.Location = new Point(363, 182);
             label1.Name = "label1";
             label1.Size = new Size(74, 15);
             label1.TabIndex = 0;
